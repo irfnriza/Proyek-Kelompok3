35c89e39267052ab173243d97395dd89aa6f8334
Tiara Agustin <132027408+tiaraagustinn@users.noreply.github.com>
2023-05-05 14:17:16 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,48 +2,63 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Command Line Argument
 int main(int argc, char const *argv[])
 {
-
+    // memastikan argumen yang diinput sesuai ketentuan
     if (argc != 3)
     {
+        // jika tidak sesuai maka tampilkan sebagai berikut
         printf("Login Gagal!\n");
         printf("Cara Login: ./FileAppProram username password");
     }
 
+    // deklarasikan variabel baru untuk menampung argumen yang diinput
     char usernameInp[30], passwordInp[30];
+    // digunakan strcpy() untuk meng-copy string dari satu variabel ke variabel lain
     strcpy(usernameInp, argv[1]);
     strcpy(passwordInp, argv[2]);
 
     FILE *fptr;
-
+    // buka file login.bin
+    // gunakan mode 'rb' untuk read binary
     if ((fptr = fopen("database/login.bin", "rb")) == NULL)
     {
         printf("No File Detected!");
+        // program akan keluar jika file tidak ditemukan
         exit(1);
     }
-    
+
+    // deklarasikan variabel acc untuk menyimpan isi dari file login.bin
     char acc[35];
+    
     fread(acc, sizeof(char), sizeof(acc), fptr);
 
+    // file fptr ditutup
     fclose(fptr);
 
+    // deklarasikan variabel baru untuk menampung sementara hasil pemisahan dari variabel acc
     char *str[3];
     char username[30], password[30];
     int i = 0;
-    
+
+    // Digunakan strtok untuk memisahkan string dari karakter (huruf) tertentu
     str[i] = strtok(acc, "$");
     while (str[i++] != NULL)
     {
         str[i] = strtok(NULL, "$");
     }
 
+    // hasil pemisahan disimpan ke dalam variabel str[0] dan str[1]
+    // Digunakan strcpy() untuk meng-copy string dari satu variabel ke variabel lain
     strcpy(username, str[0]);
     strcpy(password, str[1]);
 
-    if((strcmp(username, usernameInp)==0)&&(strcmp(password, passwordInp)==0)){
-        printf("Anda Berhasil Login!");
-        
+    // Gunakan strcmp() untuk membandingkan string dengan string yang lainnya
+    if ((strcmp(username, usernameInp) == 0) && (strcmp(password, passwordInp) == 0))
+    {
+        printf("Anda Berhasil Login!\n");
+        // jika login berhasil, maka akan langsung masuk ke dalam permainan
         char jawaban;
     int poin[]= {0,0,0};
     
